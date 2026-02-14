c9e3c37cd54ed594420ec6fd20b1968587c159ed
eduardo-hdez <ehdez2004@gmail.com>
2026-02-13 22:47:52 -0600

    [Build] add get_all_items controller

--- app/controllers/item_controller.py
+++ app/controllers/item_controller.py
@@ -7,7 +7,18 @@ async def create_item(item_data: ItemCreate):
     item_id = str(uuid.uuid4())
     item_dict = item_data.dict()
     item_dict["id"] = item_id
+    
     # Insert into MongoDB
     await items_collection.insert_one(item_dict)
 
     return item_dict
+
+async def get_all_items():
+    # Retrieve all items with limit of 100
+    items = await items_collection.find().to_list(100)
+     
+    # Remove MongoDB ID from each item
+    for item in items:
+        item.pop("_id", None)
+    
+    return items
