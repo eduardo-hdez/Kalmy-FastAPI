b6575e5659c420b8a3935ce6881fdd430c986ce9
eduardo-hdez <ehdez2004@gmail.com>
2026-02-13 22:52:40 -0600

    [Build] add get_item_by_id controller

--- app/controllers/item_controller.py
+++ app/controllers/item_controller.py
@@ -7,7 +7,7 @@ async def create_item(item_data: ItemCreate):
     item_id = str(uuid.uuid4())
     item_dict = item_data.dict()
     item_dict["id"] = item_id
-    
+
     # Insert into MongoDB
     await items_collection.insert_one(item_dict)
 
@@ -22,3 +22,13 @@ async def get_all_items():
         item.pop("_id", None)
     
     return items
+
+async def get_item_by_id(item_id: str):
+    # Retrieve item by id
+    item = await items_collection.find_one({"id": item_id})
+
+    # Remove MongoDB ID from item
+    if item:
+        item.pop("_id", None)
+
+    return item
